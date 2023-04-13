c4fd9d913e0d7385dc1ddf871f0ec59b5a3239d5
hawkin-dono <hieuvut123@gmail.com>
2023-04-13 11:13:45 +0700

    add ending

--- src/uet/oop/bomberman/Game.java
+++ src/uet/oop/bomberman/Game.java
@@ -13,10 +13,10 @@ import java.awt.image.BufferStrategy; // import class BufferStrategy để tối
 import java.awt.image.BufferedImage; // import class BufferedImage để lưu trữ hình ảnh	(2)
 import java.awt.image.DataBufferInt; // import class DataBufferInt để thao tác trực tiếp với các pixel trong BufferedImage (3)
 
-public class Game extends Canvas { 
+public class Game extends Canvas {
 	// Khởi tạo class Game, kế thừa class Canvas (để vẽ hình lên)
 	public static final int TILES_SIZE = 16, // Kích thước của một ô trong game
-			WIDTH = TILES_SIZE * (31/2), // Độ rộng của game (31 ô)
+			WIDTH = TILES_SIZE * (31 / 2), // Độ rộng của game (31 ô)
 			HEIGHT = 13 * TILES_SIZE; // Độ cao của game (13 ô)
 
 	public static int SCALE = 3; // Tỉ lệ thu phóng game khi vẽ
@@ -47,167 +47,161 @@ public class Game extends Canvas {
 	private Frame _frame; // Đối tượng cửa sổ game
 
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB); // Tạo ảnh đệm để render
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData(); // Lấy ra các pixel của ảnh đệm
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData(); // Lấy ra các pixel của ảnh đệm
 
 	public Game(Frame frame) {
-	// Gán giá trị cho thuộc tính _frame
-	_frame = frame;
-	// Thiết lập tiêu đề cho frame
-	_frame.setTitle(TITLE);
-	// Khởi tạo đối tượng Screen với kích thước rộng và cao
-	screen = new Screen(WIDTH, HEIGHT);
-	// Khởi tạo đối tượng Keyboard để xử lý các sự kiện phím
-	_input = new Keyboard();
-	
-	// Khởi tạo đối tượng Board với tham số là Game(this), Keyboard và Screen
-	_board = new Board(this, _input, screen);
-	// Đăng ký bộ xử lí  phím với đối tượng Board
-	addKeyListener(_input);
+		_frame = frame;
+		_frame.setTitle(TITLE);
+		screen = new Screen(WIDTH, HEIGHT);
+		_input = new Keyboard();
+
+		_board = new Board(this, _input, screen);
+		addKeyListener(_input);
 	}
-	// Phương thức vẽ game lên màn hình
+
 	private void renderGame() {
-	// Lấy đối tượng BufferStrategy để xử lý double buffering
-	BufferStrategy bs = getBufferStrategy();
-	if(bs == null) {
-	createBufferStrategy(3);
-	return;
-	}
-	// Xóa màn hình
-	screen.clear();
-		
-	// Vẽ Board lên màn hình
-	_board.render(screen);
-		
-	// Sao chép dữ liệu từ Screen vào mảng pixels để vẽ lên màn hình
-	for (int i = 0; i < pixels.length; i++) {
+		// Lấy đối tượng BufferStrategy để xử lý double buffering
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		// Xóa màn hình
+		screen.clear();
+
+		// Vẽ Board lên màn hình
+		_board.render(screen);
+
+		// Sao chép dữ liệu từ Screen vào mảng pixels để vẽ lên màn hình
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen._pixels[i];
-	}
-		
-	// Lấy đối tượng Graphics từ BufferStrategy để vẽ lên màn hình
-	Graphics g = bs.getDrawGraphics();
-		
-	// Vẽ ảnh lên màn hình
-	g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-	// Vẽ các thông báo lên màn hình
-	_board.renderMessages(g);
-		
-	// Giải phóng đối tượng Graphics
-	g.dispose();
-	// Hiển thị đối tượng BufferStrategy
-	bs.show();
+		}
+
+		// Lấy đối tượng Graphics từ BufferStrategy để vẽ lên màn hình
+		Graphics g = bs.getDrawGraphics();
+
+		// Vẽ ảnh lên màn hình
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		// Vẽ các thông báo lên màn hình
+		_board.renderMessages(g);
+
+		// Giải phóng đối tượng Graphics
+		g.dispose();
+		// Hiển thị đối tượng BufferStrategy
+		bs.show();
 	}
 
 	// Phương thức vẽ màn hình
 	private void renderScreen() {
-	// Lấy đối tượng BufferStrategy để xử lý double buffering
-	BufferStrategy bs = getBufferStrategy();
-	if(bs == null) {
-	createBufferStrategy(3);
-	return;
-	}
-	// Xóa màn hình
-	screen.clear();
-		
-	// Lấy đối tượng Graphics từ BufferStrategy để vẽ lên màn hình
-	Graphics g = bs.getDrawGraphics();
-		
-	// Vẽ màn hình
-	_board.drawScreen(g);
-	// Giải phóng đối tượng Graphics
-	g.dispose();
-	// Hiển thị đối tượng BufferStrategy
-	bs.show();
+		// Lấy đối tượng BufferStrategy để xử lý double buffering
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		// Xóa màn hình
+		screen.clear();
+
+		// Lấy đối tượng Graphics từ BufferStrategy để vẽ lên màn hình
+		Graphics g = bs.getDrawGraphics();
+
+		// Vẽ màn hình
+		_board.drawScreen(g);
+		// Giải phóng đối tượng Graphics
+		g.dispose();
+		// Hiển thị đối tượng BufferStrategy
+		bs.show();
 	}
 
 	// Phương thức cập nhật trạng thái của game
 	private void update() {
-	// Cập nhật trạng thái của bàn phím
-	_input.update();
-	// Cập nhật trạng thái của Board
-	_board.update();
+		// Cập nhật trạng thái của bàn phím
+		_input.update();
+		// Cập nhật trạng thái của Board
+		_board.update();
 	}
 	// Bắt đầu vòng lặp game
 	public void start() {
-	_running = true;
-	// Khởi tạo thời gian và tốc độ khung hình
-	long lastTime = System.nanoTime();
-	long timer = System.currentTimeMillis();
-	final double ns = 1000000000.0 / 60.0; // nanosecond, 60 frames per second
-	double delta = 0;
-	int frames = 0;
-	int updates = 0;
+		_running = true;
 
-	// Yêu cầu trỏ chuột vào cửa sổ game
-	requestFocus();
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0; // nanosecond, 60 frames per second
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 
-	// Vòng lặp chính của game
-	while(_running) {
-		long now = System.nanoTime();
-		delta += (now - lastTime) / ns;
-		lastTime = now;
-		
-		// Cập nhật trạng thái game và đếm số lần cập nhật
-		while(delta >= 1) {
-			update();
-			updates++;
-			delta--;
-		}
-		
-		// Nếu game đang tạm dừng
-		if(_paused) {
-			// Nếu thời gian tạm dừng đã hết, tiếp tục hiển thị game
-			if(_screenDelay <= 0) {
-				_board.setShow(-1);
-				_paused = false;
+		// Yêu cầu trỏ chuột vào cửa sổ game
+		requestFocus();
+
+		// Vòng lặp chính của game
+		while (_running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			// Cập nhật trạng thái game và đếm số lần cập nhật
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
 			}
-			
-			// Hiển thị màn hình tạm dừng
-			renderScreen();
-		} else {
-			// Hiển thị game
-			renderGame();
+
+			// Nếu game đang tạm dừng
+			if (_paused) {
+				// Nếu thời gian tạm dừng đã hết, tiếp tục hiển thị game
+				if (_screenDelay <= 0) {
+					_board.setShow(-1);
+					_paused = false;
+				}
+
+				// Hiển thị màn hình tạm dừng
+				renderScreen();
+			} else {
+				// Hiển thị game
+				renderGame();
+			}
+
+			// Đếm số lần hiển thị khung hình và cập nhật tiêu đề cửa sổ
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				_frame.setTime(_board.subtractTime());
+				_frame.setPoints(_board.getPoints());
+				timer += 1000;
+				_frame.setTitle(TITLE + " | " + updates + " rate, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+
+				// Giảm độ trễ hiển thị màn hình tạm dừng nếu cần
+				if (_board.getShow() == 2)
+					--_screenDelay;
+			}
 		}
-		
-		// Đếm số lần hiển thị khung hình và cập nhật tiêu đề cửa sổ
-		frames++;
-		if(System.currentTimeMillis() - timer > 1000) {
-			_frame.setTime(_board.subtractTime());
-			_frame.setPoints(_board.getPoints());
-			timer += 1000;
-			_frame.setTitle(TITLE + " | " + updates + " rate, " + frames + " fps");
-			updates = 0;
-			frames = 0;
-			
-			// Giảm độ trễ hiển thị màn hình tạm dừng nếu cần
-			if(_board.getShow() == 2)
-				--_screenDelay;
-		}
-	}
 	}
 
 	// Lấy tốc độ của nhân vật di chuyển
 	public static double getBomberSpeed() {
-	return bomberSpeed;
+		return bomberSpeed;
 	}
 
 	// Lấy số lượng bom có thể đặt cùng một lúc
 	public static int getBombRate() {
-	return bombRate;
+		return bombRate;
 	}
 
 	// Lấy bán kính của vùng bị ảnh hưởng bởi bom
 	public static int getBombRadius() {
-	return bombRadius;
+		return bombRadius;
 	}
 
 	// Tăng tốc độ di chuyển của nhân vật
 	public static void addBomberSpeed(double i) {
-	bomberSpeed += i;
+		bomberSpeed += i;
 	}
 
 	// Tăng bán kính của vùng bị ảnh hưởng bởi bom
 	public static void addBombRadius(int i) {
-	bombRadius += i;
+		bombRadius += i;
 	}
 
 	// Tăng số lượng bom có thể đặt cùng một lúc
@@ -225,20 +219,20 @@ public class Game extends Canvas {
 	public boolean isPaused() { // Trả về trạng thái đang tạm dừng trò chơi hay không
 		return _paused;
 	}
-	
+
 	public void pause() { // Tạm dừng trò chơi
 		_paused = true;
 	}
-	
+
 	public static void setBombRate(int bombRate) { // Thiết lập tốc độ thả bomb của nhân vật
-        Game.bombRate = bombRate;
-    }
+		Game.bombRate = bombRate;
+	}
 
-    public static void setBombRadius(int bombRadius) { // Thiết lập bán kính tác động của bomb
-        Game.bombRadius = bombRadius;
-    }
+	public static void setBombRadius(int bombRadius) { // Thiết lập bán kính tác động của bomb
+		Game.bombRadius = bombRadius;
+	}
 
-    public static void setBomberSpeed(double bomberSpeed) { // Thiết lập tốc độ di chuyển của nhân vật
-        Game.bomberSpeed = bomberSpeed;
-    }
+	public static void setBomberSpeed(double bomberSpeed) { // Thiết lập tốc độ di chuyển của nhân vật
+		Game.bomberSpeed = bomberSpeed;
+	}
 }
