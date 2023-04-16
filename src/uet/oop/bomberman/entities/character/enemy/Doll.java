ee70515629fcc46049f4dfdb61d4933bb5c8c8db
hawkin-dono <hieuvut123@gmail.com>
2023-04-16 18:20:59 +0700

    Update

--- src/uet/oop/bomberman/entities/character/enemy/Doll.java
+++ src/uet/oop/bomberman/entities/character/enemy/Doll.java
@@ -1,25 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uet.oop.bomberman.entities.character.enemy;
 
 import uet.oop.bomberman.Board;
-import uet.oop.bomberman.entities.character.enemy.ai.AIMedium;
+import uet.oop.bomberman.entities.character.enemy.ai.AILow;
 import uet.oop.bomberman.graphics.Sprite;
 
-/**
- *
- */
 public class Doll extends Enemy{
-
     public Doll(int x, int y, Board board) {
         super(x, y, board, Sprite.doll_dead, 0.8, 100);
 
         _sprite = Sprite.doll_left1;
 
-        _ai = new AIMedium(_board.getBomber(), this);
+        _ai = new AILow();
         _direction = _ai.calculateDirection();
 
     }
@@ -27,22 +18,10 @@ public class Doll extends Enemy{
     @Override
     protected void chooseSprite() {
         switch (_direction) {
-            case 0:
-            case 1:
-                if (_moving) {
+            case 0, 1 ->
                     _sprite = Sprite.movingSprite(Sprite.doll_right1, Sprite.doll_right2, Sprite.doll_right3, _animate, 60);
-                } else {
-                    _sprite = Sprite.doll_left1;
-                }
-                break;
-            case 2:
-            case 3:
-                if (_moving) {
+            case 2, 3 ->
                     _sprite = Sprite.movingSprite(Sprite.doll_left1, Sprite.doll_left2, Sprite.doll_left3, _animate, 60);
-                } else {
-                    _sprite = Sprite.doll_left1;
-                }
-                break;
         }
     }
 }
