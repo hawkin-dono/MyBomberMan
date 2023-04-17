1aa3689bf0477f88acf3fa28572ad8040798d5e0
hoanghelloword <115699781+hoanghelloword@users.noreply.github.com>
2023-04-17 09:09:30 +0700

    Update BombermanGame.java

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -4,6 +4,7 @@ import uet.oop.bomberman.gui.Frame;
 
 public class BombermanGame {
 	public static void main(String[] args) {
+		Sound.playSound("res/sound/soundtrack.wav",1000);
 		new Frame();
 	}
 }
